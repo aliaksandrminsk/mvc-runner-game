fa52e0eb773d35cdc09fcbbce9fdfb2afca1fc06
Aliaksandr <gadzinik@gmail.com>
2022-09-27 02:54:38 +0300

    for test

--- src/App.ts
+++ src/App.ts
@@ -34,6 +34,8 @@ export class App {
     const gameView = new GameView();
     new GameController(gameModel, gameView);
 
+    vf fgfgfgfg
+
     // Size and resize game.
     window.addEventListener("resize", () => this.resize());
     this.resize();
